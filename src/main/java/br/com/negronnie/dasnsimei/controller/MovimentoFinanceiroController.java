91d644efc125597125f44709fa6b67c748ba5d59
Lucas Ponciano <lucaswponciano@gmail.com>
2026-03-17 17:13:15 -0300

    Controller passa a receber arquivo ao invés de um caminho

--- src/main/java/br/com/negronnie/dasnsimei/controller/MovimentoFinanceiroController.java
+++ src/main/java/br/com/negronnie/dasnsimei/controller/MovimentoFinanceiroController.java
@@ -2,12 +2,15 @@ package br.com.negronnie.dasnSimei.controller;
 
 import br.com.negronnie.dasnSimei.model.entities.MovimentoFinanceiro;
 import br.com.negronnie.dasnSimei.repositories.MovimentoFinanceiroRepository;
+import br.com.negronnie.dasnSimei.services.MovimentoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -17,10 +20,17 @@ public class MovimentoFinanceiroController {
     @Autowired
     private MovimentoFinanceiroRepository movimentoFinanceiroRepository;
 
-    @PostMapping
-    public MovimentoFinanceiro novoMovimento(@Valid MovimentoFinanceiro movimentoFinanceiro) {
-        movimentoFinanceiroRepository.save(movimentoFinanceiro);
-        return movimentoFinanceiro;
+    @Autowired
+    private MovimentoService service;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile arquivo){
+        try {
+            service.processarArquivoCSV(arquivo);
+            return "OK";
+        } catch (IOException e) {
+            return "Erro: " + e.getMessage();
+        }
     }
 
     @GetMapping
@@ -28,12 +38,12 @@ public class MovimentoFinanceiroController {
         return movimentoFinanceiroRepository.findAll();
     }
 
-    @GetMapping(path="/{id}")
+    @GetMapping("/{id}")
     public Optional<MovimentoFinanceiro> obterMovimento(@PathVariable Long id){
         return movimentoFinanceiroRepository.findById(id); // .orElse(null) sem usar optional
     }
 
-    @GetMapping(path = "/pagina/{numeroPagina}")
+    @GetMapping("/pagina/{numeroPagina}")
     public Iterable<MovimentoFinanceiro> obterMovimentosPagina(@PathVariable int numeroPagina){
         Pageable page = PageRequest.of(numeroPagina, 10);
         return movimentoFinanceiroRepository.findAll(page);
