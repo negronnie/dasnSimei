82304296b99a8806be0bb6c64463d83e0c2a4f87
Lucas Ponciano <lucaswponciano@gmail.com>
2026-03-24 19:18:35 -0300

    Alterado a herança para JpaRepository, para poder usar lista no lugar de iterable.

--- src/main/java/br/com/negronnie/dasnsimei/repositories/MovimentoFinanceiroRepository.java
+++ src/main/java/br/com/negronnie/dasnsimei/repositories/MovimentoFinanceiroRepository.java
@@ -1,14 +1,13 @@
 package br.com.negronnie.dasnSimei.repositories;
 
 import br.com.negronnie.dasnSimei.model.entities.MovimentoFinanceiro;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 
-public interface MovimentoFinanceiroRepository extends PagingAndSortingRepository<MovimentoFinanceiro, Long>, CrudRepository<MovimentoFinanceiro, Long> {
+public interface MovimentoFinanceiroRepository extends JpaRepository<MovimentoFinanceiro, Long> {
 
     @Query("select sum(m.valor) from MovimentoFinanceiro m where year(m.data) = :ano")
     BigDecimal obterTotalAnual(@Param("ano") int ano);
@@ -20,6 +19,5 @@ public interface MovimentoFinanceiroRepository extends PagingAndSortingRepositor
     BigDecimal obterTotalTrimestre(@Param("ano") int ano, @Param("mesInicial") int mesInicial, @Param("mesFinal") int mesFinal);
 
     @Query("select sum(m.valor) from MovimentoFinanceiro m where (:categoria = 'vendas' and type(m) = VendaExterna ) or (:categoria = 'previsao' and type(m) = Previsao )")
-//    @Query("select sum(m.valor) from MovimentoFinanceiro m where type(m) = :categoria") por que isso aqui não funciona?
     BigDecimal obterTotalCategoria(@Param("categoria") String categoria);
 }
