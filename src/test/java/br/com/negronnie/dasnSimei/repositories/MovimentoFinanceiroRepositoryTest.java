f7aa7cf070673df95ffbdbe4e1e7ed8b414bedc5
Lucas Ponciano <lucaswponciano@gmail.com>
2026-04-15 20:22:36 -0300

    Refactor dos testes removendo boilerplate através do BeforeEach.

--- src/test/java/br/com/negronnie/dasnSimei/repositories/MovimentoFinanceiroRepositoryTest.java
+++ src/test/java/br/com/negronnie/dasnSimei/repositories/MovimentoFinanceiroRepositoryTest.java
@@ -3,6 +3,7 @@ package br.com.negronnie.dasnSimei.repositories;
 import br.com.negronnie.dasnSimei.model.entities.Movimento;
 import br.com.negronnie.dasnSimei.model.entities.MovimentoFinanceiro;
 import br.com.negronnie.dasnSimei.model.entities.VendaExterna;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +21,21 @@ class MovimentoFinanceiroRepositoryTest {
     @Autowired
     private MovimentoFinanceiroRepository repository;
 
-    @Test
-    @DisplayName("Persistir Movimento Financeiro")
-    void testMovimentoFinanceiro_quandoForSalvo_devePersistirNoBanco() {
-        Movimento mov = new Movimento(
+    private Movimento mov;
+
+    @BeforeEach
+    public void setup() {
+        mov = new Movimento(
                 LocalDate.of(2025, 1, 1),
                 new BigDecimal("2157.03"),
                 "Tasdoifjsdf",
                 "Movimento financeiro");
+    }
+
+    @Test
+    @DisplayName("Persistir Movimento Financeiro")
+    void testMovimentoFinanceiro_quandoForSalvo_devePersistirNoBanco() {
+
 
         Movimento movimentoSalvo = repository.save(mov);
         assertNotNull(movimentoSalvo);
@@ -40,18 +48,13 @@ class MovimentoFinanceiroRepositoryTest {
     @Test
     @DisplayName("Retornar Lista de Movimentos Financeiros")
     void testMovimentoFinanceiro_quandoConsultar_deveTrazerListaDeMovimentos() {
-        Movimento mov1 = new Movimento(
-                LocalDate.of(2025, 1, 1),
-                new BigDecimal("2157.03"),
-                "Tasdoifjsdf",
-                "Movimento financeiro");
         Movimento mov2 = new Movimento(
                 LocalDate.of(2026, 9, 24),
                 new BigDecimal("6894.12"),
                 "soiergjdlergt",
                 "Venda de produto");
 
-        repository.save(mov1);
+        repository.save(mov);
         repository.save(mov2);
 
         List<MovimentoFinanceiro> listaMovimentos = repository.findAll();
@@ -63,33 +66,21 @@ class MovimentoFinanceiroRepositoryTest {
     @Test
     @DisplayName("Retornar Movimento por ID")
     void testMovimentoFinanceiro_quandoReceberId_deveRetornarMovimentoComID() {
-        Movimento mov1 = new Movimento(
-                LocalDate.of(2025, 1, 1),
-                new BigDecimal("2157.03"),
-                "Tasdoifjsdf",
-                "Movimento financeiro");
-
-        repository.save(mov1);
-        MovimentoFinanceiro movSalvo = repository.findById(mov1.getId()).get();
+        repository.save(mov);
+        MovimentoFinanceiro movSalvo = repository.findById(mov.getId()).get();
 
         assertNotNull(movSalvo);
-        assertEquals(movSalvo.getId(), mov1.getId());
+        assertEquals(movSalvo.getId(), mov.getId());
     }
 
     @Test
     @DisplayName("Retornar Valor Total do Ano Selecionado")
     void testObterTotalAnual() {
-        Movimento mov1 = new Movimento(
-                LocalDate.of(2025, 1, 1),
-                new BigDecimal("2157.03"),
-                "Tasdoifjsdf",
-                "Movimento financeiro");
-
-        repository.save(mov1);
+        repository.save(mov);
         BigDecimal totalAno = repository.obterTotalAnual(2025);
 
         assertNotNull(totalAno);
-        assertEquals(totalAno, mov1.getValor());
+        assertEquals(totalAno, mov.getValor());
     }
 
     @Test
@@ -111,17 +102,11 @@ class MovimentoFinanceiroRepositoryTest {
     @Test
     @DisplayName("Retornar Valor Total do Trimestre Selecionado")
     void testObterTotalTrimestre() {
-        Movimento mov1 = new Movimento(
-                LocalDate.of(2025, 2, 1),
-                new BigDecimal("2157.03"),
-                "Tasdoifjsdf",
-                "Movimento financeiro");
-
-        repository.save(mov1);
+        repository.save(mov);
         BigDecimal totalTrimestre = repository.obterTotalTrimestre(2025,1,3);
 
         assertNotNull(totalTrimestre);
-        assertEquals(totalTrimestre, mov1.getValor());
+        assertEquals(totalTrimestre, mov.getValor());
     }
 
     @Test
@@ -143,19 +128,13 @@ class MovimentoFinanceiroRepositoryTest {
     @Test
     @DisplayName("Retornar Movimento Financeiro Contendo Texto Parcialmente")
     void testFindMovimentoFinanceiroByDescricaoContainingIgnoreCase() {
-        Movimento mov1 = new Movimento(
-                LocalDate.of(2025, 1, 1),
-                new BigDecimal("2157.03"),
-                "Tasdoifjsdf",
-                "Movimento financeiro");
-
         String statement = "Finan";
-        repository.save(mov1);
+        repository.save(mov);
 
         List <MovimentoFinanceiro> movSalvos = repository.findMovimentoFinanceiroByDescricaoContainingIgnoreCase(statement);
 
         assertNotNull(movSalvos);
         assertEquals(1, movSalvos.size());
-        assertEquals(mov1.getDescricao().contains(statement), movSalvos.get(0).getDescricao().contains(statement));
+        assertEquals(mov.getDescricao().contains(statement), movSalvos.get(0).getDescricao().contains(statement));
     }
 }
