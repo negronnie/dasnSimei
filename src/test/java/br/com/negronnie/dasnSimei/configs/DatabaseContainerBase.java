d6e680177ed88b2aea86f947c9fb84b81c25a32d
Lucas Ponciano <lucaswponciano@gmail.com>
2026-04-22 18:17:50 -0300

    Comentários sobre o uso das anotações @Testcontainers, @Container e @ServiceConnection

--- src/test/java/br/com/negronnie/dasnSimei/configs/DatabaseContainerBase.java
+++ src/test/java/br/com/negronnie/dasnSimei/configs/DatabaseContainerBase.java
@@ -1,17 +1,24 @@
 package br.com.negronnie.dasnSimei.configs;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
 
+//@Testcontainers
 public abstract class DatabaseContainerBase {
 
+//    @Container
+//    @ServiceConnection
     @SuppressWarnings("resource")
     static MySQLContainer mysql = new MySQLContainer("mysql:9.5.0");
 
     static {
         mysql.start();
     }
+    // Com Container, Service Connection e TestContainers comentados, eu posso gerenciar o ciclo de vida do container. Isso pode alterar o tempo de execução dos testes.
 
     @DynamicPropertySource
     static void overrideDatasource(DynamicPropertyRegistry registry) {
