67336525d1ac37dd8ec58657cae79701e8b3f2c0
Lucas Ponciano <lucaswponciano@gmail.com>
2026-04-22 20:47:59 -0300

    Delegado ao Testcontainers a gerência do ciclo de vida do Container de banco de dados. Desta forma os testes ficam ligeramente mais lentos, mas se mantêm dentro do princípio de isolamento dos testes.

--- src/test/java/br/com/negronnie/dasnSimei/configs/DatabaseContainerBase.java
+++ src/test/java/br/com/negronnie/dasnSimei/configs/DatabaseContainerBase.java
@@ -1,22 +1,26 @@
 package br.com.negronnie.dasnSimei.configs;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
 
-//@Testcontainers
+@Testcontainers
 public abstract class DatabaseContainerBase {
 
-//    @Container
-//    @ServiceConnection
+    @Container
+    @ServiceConnection
     @SuppressWarnings("resource")
     static MySQLContainer mysql = new MySQLContainer("mysql:9.5.0");
 
-    static { // Senti que dessa forma, o tempo de execução ficou mais rápido no geral.
-        mysql.start();
-    }
+//    static {
+//        mysql.start();
+//    }
     // Com Container, Service Connection e TestContainers comentados, eu posso gerenciar o ciclo de vida do container.
     // Isso pode alterar o tempo de execução dos testes.
+    // Senti que dessa forma, o tempo de execução ficou mais rápido no geral. Mas quebrou o meu repository
 
     @DynamicPropertySource
     static void overrideDatasource(DynamicPropertyRegistry registry) {
