7c07e10d1dfa226da537b3185349a88aba7781ce
Lucas Ponciano <lucaswponciano@gmail.com>
2026-04-22 18:01:09 -0300

    Implementação do primeiro Teste de Integração.

---
+++ src/test/java/br/com/negronnie/dasnSimei/integration/SwaggerIntegrationTest.java
@@ -0,0 +1,31 @@
+package br.com.negronnie.dasnSimei.integration;
+
+import br.com.negronnie.dasnSimei.configs.AbstractIntegrationTest;
+import br.com.negronnie.dasnSimei.configs.TestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+	@Test
+	@DisplayName("Publicação da Página do Swagger")
+	void testDeveCarregarPaginaDoSwagger() {
+
+		var conteudo = given()
+			.basePath("/swagger-ui/index.html")
+			.port(TestConfig.SERVER_PORT)
+			.when()
+				.get()
+			.then()
+				.statusCode(200)
+			.extract()
+				.body().asString();
+
+		assertTrue(conteudo.contains("Swagger UI"));
+
+	}
+
+}
