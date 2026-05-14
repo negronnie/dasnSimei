3eba88ec4df8fb38bf37860a6b058b6f60d3d8a8
Lucas Ponciano <lucaswponciano@gmail.com>
2026-05-14 20:58:02 -0300

    Ajustes nas configurações de Teste

--- src/test/java/br/com/negronnie/dasnSimei/configs/TestConfig.java
+++ src/test/java/br/com/negronnie/dasnSimei/configs/TestConfig.java
@@ -5,4 +5,8 @@ public class TestConfig {
     public static final int SERVER_PORT = 8888;
     public static final String CONTENT_TYPE_JSON = "application/json";
 
+    // Testes de headers/cors
+    public String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public String HEADER_PARAM_ORIGIN = "Origin";
+
 }
